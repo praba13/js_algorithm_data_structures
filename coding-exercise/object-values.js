13fef4e748c21e27f821533aba1db5c368716f4e
Praba <prama1303@gmail.com>
2023-04-25 00:03:06 +0200

    object values

---
+++ coding-exercise/object-values.js
@@ -0,0 +1,10 @@
+const credits = {
+  producer: 'Open Replay',
+  editor: 'Federico',
+  assistant: 'John'
+};
+
+const arr = Object.values(credits);
+console.log(arr);
+
+//Output: ["Open Replay", "Federico", "John"];
