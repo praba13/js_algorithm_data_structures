7e0805d3ea90d0724a09cae2ace0cd03b6d8e190
Praba <prama1303@gmail.com>
2023-05-12 22:42:16 +0200

    implement shorthand small programms

---
+++ shorthand/short.js
@@ -0,0 +1,17 @@
+//Longhand
+let x;
+let y = 20;
+
+//Shorthand
+let x,
+  y = 20;
+
+//////////////////////////
+//Longhand
+let a, b, c;
+a = 5;
+b = 8;
+c = 12;
+
+//Shorthand
+let [a, b, c] = [5, 8, 12];
