fc33839f3155584e9840a8979ee33b77fca70408
Praba <prama1303@gmail.com>
2023-06-09 23:37:46 +0200

    shorthands if case

--- shorthand/shortMore.js
+++ shorthand/shortMore.js
@@ -83,3 +83,56 @@ if (likeJavaScript === true) {
 if (likeJavaScript) {
   // Shorthand
 }
+
+/*
+//Switch case alternative
+const dayOfWeek = 3;
+
+switch (dayOfWeek) {
+  case 1:
+    document.write("It's Monday");
+    break;
+  case 2:
+    document.write("It's Tuesday");
+    break;
+  case 3:
+    document.write("It's Wednesday");
+    break;
+  case 4:
+    document.write("It's Thursday");
+    break;
+  case 5:
+    document.write("It's Friday");
+    break;
+}
+*/
+
+// shorthand
+
+/*
+const case = {
+  'one': () => console.log('It is Monday'),
+  'two': () => console.log('It is Tuesday'),
+  'three': () => console.log('It is Wednesday'),  
+  'four': () => console.log('It is Thursday'),  
+  'five': () => console.log('It is Friday')
+};
+
+(case[dayOfWeek] || case['three'])();
+
+*/
+
+// longhand
+// joining arrays
+const nums1 = [0,1,2,3];
+const nums2 = [4,5,6].concat(nums1);
+// cloning arrays
+const arr = [0,1,2,3,4,5];
+const arr2 = arr.slice()
+//shorthand
+// joining arrays
+const nums1 = [0,1,2,3 ];
+const nums2 = [4,5,6, …nums1];
+// cloning arrays
+const arr = [1, 2, 3, 4,5];
+const arr2 = […arr];
